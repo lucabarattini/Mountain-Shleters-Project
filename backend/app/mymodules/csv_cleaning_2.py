b6381eb47e827b098ef00ce576a97deb8885a80f
Manuel Okoye <manuel.okoye@student.h-farm.com>
2023-11-27 11:54:50 +0100

    added a function to clean the second csv

---
+++ backend/app/mymodules/csv_cleaning_2.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def process_data(file_path):
+    # Read the CSV file
+    df = pd.read_csv(file_path)
+
+    # Drop the first two columns
+    df.drop(df.columns[0:2], axis=1, inplace=True)
+
+    # List of column indices to keep
+    columns_to_keep = [0, 1, 4, 7, 9, 12, 14, 15, 16]
+
+    # Keep only the specified columns and reset the index
+    df = df.iloc[:, columns_to_keep].reset_index(drop=True)
+    
+    # Replace NaN values with "---"
+    df.fillna("---", inplace=True)
+
+    return df
+
+# Call the function and store the result in a variable
+processed_df = process_data(file_path)
+
+# Display the modified DataFrame
+print(processed_df)

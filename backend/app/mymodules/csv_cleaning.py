7d5fd8a44647e2b044788ce953bb939a9d30ad21
Manuel Okoye <manuel.okoye@student.h-farm.com>
2023-12-08 11:10:05 +0100

    Check issue #15. Various enhancments mainly on the csv_cleaning function inside the mymodules folder.

--- backend/app/mymodules/csv_cleaning.py
+++ backend/app/mymodules/csv_cleaning.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def cleancsv1(file_path, delimiter=';'):
     data = pd.read_csv(file_path, delimiter=delimiter)
@@ -8,22 +9,31 @@ def cleancsv1(file_path, delimiter=';'):
 
     # Dropping columns
     drop_indexes = [0, 1, 4, 5, 8, 9, 40] + list(range(11, 39))
-
     data.drop(data.columns[drop_indexes], axis=1, inplace=True)
     
     # Dropping all columns after index 43
     data = data.iloc[:, :8]
 
     # Moving 'Denominazione Struttura' to index 0
-    # Ensure the column name is in uppercase as the names have been converted
     denominazione_col = data.pop('DENOMINAZIONESTRUTTURA')
     data.insert(0, 'DENOMINAZIONE', denominazione_col)
 
-    # Replace NaN values with '---'
-    data.fillna('---', inplace=True)
+    # Drop duplicate entries based on 'DENOMINAZIONE'
+    data.drop_duplicates(subset='DENOMINAZIONE', keep='first', inplace=True)
 
     # Rename the last three columns
     new_column_names = {data.columns[5]: 'CAMERE', data.columns[6]: 'LETTI', data.columns[7]: 'BAGNI'}
     data.rename(columns=new_column_names, inplace=True)
+
+    # Fill NaN values in integer columns with 0 and then change the format to integer
+    integer_columns = ['CAMERE', 'LETTI', 'BAGNI']
+    data[integer_columns] = data[integer_columns].fillna(0).astype(int)
+
+    # Replace NaN values with '---'
+    data.fillna('---', inplace=True)
+
+    # Replace phone numbers that don't have at least 10 digits with '---'
+    data['TELEFONO'] = data['TELEFONO'].apply(lambda x: x if pd.isna(x) or len(str(x)) >= 10 else '---')
+
     print(data)
     return data

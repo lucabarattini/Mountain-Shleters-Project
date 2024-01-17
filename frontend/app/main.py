b3f166f5b5d1e4d927711b79bda295cfc15927d0
Manuel Okoye <manuel.okoye@student.h-farm.com>
2024-01-17 19:07:22 +0100

    Made all code adherent to PEP8

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,26 +1,29 @@
 """
 Frontend module for the Flask application.
 
-This module defines a simple Flask application that serves as the frontend for the project.
+This module defines a simple Flask application that serves
+as the frontend for the project.
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, reques
 import requests
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  
+app.config['SECRET_KEY'] = 'your_secret_key'
 
 # Configuration for the FastAPI backend URL
-FASTAPI_BACKEND_HOST = 'http://backend'  
+FASTAPI_BACKEND_HOST = 'http://backend'
 BACKEND_URL = f'{FASTAPI_BACKEND_HOST}/query/'
+
 
 class QueryForm(FlaskForm):
     """Form for submitting queries to the backend."""
 
     person_name = StringField('Person Name:')
     submit = SubmitField('Get Birthday from FastAPI Backend')
+
 
 @app.route('/')
 def index():
@@ -32,12 +35,13 @@ def index():
     """
     return render_template('index.html')
 
+
 @app.route('/piemonte')
 def piemonte():
     """
     Render the Piemonte page with dynamic content based on query parameters.
 
-    This function fetches data from the backend based on query parameters 
+    This function fetches data from the backend based on query parameters
     provided by the user and renders the Piemonte page with this data.
 
     Returns:
@@ -55,17 +59,21 @@ def piemonte():
     }
 
     # Fetch data from the backend with query parameters
-    response = requests.get('http://backend:80/cleaned_csv_show', params=query_params)
+    response = requests.get(
+        'http://backend:80/cleaned_csv_show',
+        params=query_params)
     response.raise_for_status()
     cleaned_data = response.json()
-    
+
     # Check for an error in the response
     if 'error' in cleaned_data:
         error_message = cleaned_data['error']
         # Render the Piemonte page with an error message
-        return render_template('piemonte.html', error_message=error_message, cleaned_data=[])
+        return render_template(
+            'piemonte.html', error_message=error_message, cleaned_data=[])
 
     return render_template('piemonte.html', cleaned_data=cleaned_data)
+
 
 @app.route('/project_description')
 def project_description():
@@ -76,5 +84,13 @@ def project_description():
 
 
 if __name__ == '__main__':
-    print("🌈 Running on http://localhost:8080")
-    app.run(host='0.0.0.0', port=80, debug=False)
+<< << << < Updated upstream
+<< << << < Updated upstream
+print("🌈 Running on http://localhost:8080")
+app.run(host='0.0.0.0', port=80, debug=False)
+== == == =
+app.run(host='0.0.0.0', port=80, debug=True)
+>>>>>> > Stashed changes
+== == == =
+app.run(host='0.0.0.0', port=80, debug=True)
+>>>>>> > Stashed changes
